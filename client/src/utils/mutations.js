a7b42684e1004d797f751375f04fa63f410b8cc3
Anusha <anushar20201@gmail.com>
2022-09-25 21:26:22 -0500

    adding mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,68 @@
+import { gql } from '@apollo/client'
+
+//LOGIN_USER query
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+//ADD_USER mustation
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+//SAVE_BOOK mutation
+export const SAVE_BOOK = gql`
+mutation saveBook($bookInfo: bookInfo!) {
+    saveBook(bookInfo: $bookInfo) {
+        _id
+        username
+        password
+        bookCount
+        savedBooks{
+            bookId
+            authors
+            description
+            title 
+            image
+            link 
+        }
+    }
+  }
+`;
+
+//REMOVE_BOOK  mutation
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        password
+        bookCount
+        savedBooks{
+            bookId
+            authors
+            description
+            title 
+            image
+            link 
+        }
+    }
+  }
+`;
